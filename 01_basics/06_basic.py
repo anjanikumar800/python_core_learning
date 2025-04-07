1857a15e17cd189b63998e03fe9106a65f168176
Anjani kumar <anjnisharma800@gmail.com>
2025-04-07 16:52:02 +0000

    ipconfig

--- 01_basics/06_basic.py
+++ 01_basics/06_basic.py
@@ -62,3 +62,58 @@ print(len(num))
 print(max(num))
 print(min(num))
 print(range(10)) # this is used in loop
+
+###########     EXERCISE       ####################33
+
+# WAP to find minimum(smallest) element $ its index from a list
+print("Enter element of list(length = 10) :")
+num = []
+for val in range(0,10,1):
+    element = int(input("Enter element of %d index : " %val))
+    num.append(element)
+print("Given list : ",num)
+print("smallest element in list is :",end=" ")
+min = min(num)
+print(min)
+print("index of smallest number in list :",end=" ")
+print(num.index(min))
+print("program executed sucessfully........")
+
+# program to find average of element of given list
+print("Enter element of list(length = 10) :")
+num = []
+for val in range(0,10,1):
+    element = int(input("Enter element of %d index : " %val))
+    num.append(element)
+print("Given list : ",num)
+sum = 0
+for el in range(0,10):
+    sum += num[el]
+length = len(num)
+avg = sum/length
+print("Average of element of given list is : ",avg)
+print("program executed sucessfully........")
+
+# program to search a given element in list
+print("Enter element of list(length = 10) :")
+num = []
+for val in range(0,10,1):
+    element = int(input("Enter element of %d index : " %val))
+    num.append(element)
+print("Given list : ",num)
+search_element = int(input("Enter element that you want to search : "))
+print("%d element found at index :"%search_element,end=" ")
+print(num.index(search_element))
+print("program executed sucessfully........")
+
+# program to count number of element in list
+print("Enter element of list(length = 10) :")
+num = []
+for val in range(0,10,1):
+    element = int(input("Enter element of %d index : " %val))
+    num.append(element)
+print("Given list : ",num)
+count_element = int(input("Enter element that to be count : "))
+total_count = num.count(count_element)
+print("%d element occur %d time in list"%(count_element,total_count))
+print("program executed sucessfully........")
